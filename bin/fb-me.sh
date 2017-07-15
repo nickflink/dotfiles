eee7ad29daf1a9644faaa857e4c4fc9b9d0a9722
Nicholas Flink <nicholas@sproutsocial.com>
2017-07-15 10:55:39 -0500

    remove unused fb scripts

--- bin/fb-me.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DB='mysql -A -h slavedb'
-
-# assumes my .bashrc...
-$DB --skip-column-names -f < <(echo "use bizeebe; select profile_access_token from fb_objects where fb_object_id=537208670 and oauth_is_valid=1 limit 1;")
